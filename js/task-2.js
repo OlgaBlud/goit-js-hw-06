08eaf78ab289f3dd0e72e51c4a9e327f4fa9479b
Olga Blud <olga.blud@gmail.com>
2024-05-01 17:55:19 +0300

    hw 6 done

---
+++ js/task-2.js
@@ -0,0 +1,29 @@
+class Storage {
+  #items;
+  constructor(params) {
+    this.#items = params;
+  }
+  getItems() {
+    return this.#items;
+  }
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    const indexItemToRemove = this.#items.indexOf(itemToRemove);
+    indexItemToRemove !== -1
+      ? this.#items.splice(indexItemToRemove, 1)
+      : this.#items;
+  }
+}
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem('Droid');
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem('Prolonger');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem('Scaner');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
